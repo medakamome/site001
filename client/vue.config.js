e286bc75a4d1cfbd092fa6368e23ba517b3ee352
小松野一紀 / KOMATSUNO，KAZUNORI <kazunori.komatsuno.jf@hitachi.com>
2018-09-28 13:37:12 +0900

    update

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,5 +1,15 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    // options...
+  baseUrl : '/site001/',
+
+  assetsDir : '/public/',
+/*
+  pages : {
+    template : 'public/index.html'
+  },
+*/
+  devServer : {
+    proxy : 'http://localhost:8000'
   }
+}
